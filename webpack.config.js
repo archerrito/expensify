9de5787b80c705c84cd1667d4c1e7ed49067b6fc
Archer Hobson <supremeoverlord@archers-mbp.attlocal.net>
2018-06-04 16:44:55 -0500

    webpack production changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,37 @@
 // entry -> output
 const path = require('path');
 
+module.exports = (env) => {
+    console.log('env', env);
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            //handle api routing via client side code
+            historyApiFallback: true
+        }
+    }
+};
+
 module.exports = {
     entry: './src/app.js',
     output: {
