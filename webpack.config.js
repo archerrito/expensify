ca11385d1891e8a74110910589ea09d8cc607a6f
Archer Hobson <supremeoverlord@archers-mbp.attlocal.net>
2018-06-05 09:43:53 -0500

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 // entry -> output
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
-    console.log('env', env);
+     const isProduction = env === 'production';
+     const CSSExtract = new ExtractTextPlugin('styles.css');
     return {
         entry: './src/app.js',
         output: {
@@ -16,48 +18,34 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                soourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                soourceMap: true
+                            }
+                        }
+                    ]
+                })
             }]
         },
-        devtool: 'cheap-module-eval-source-map',
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             //handle api routing via client side code
             historyApiFallback: true
         }
-    }
-};
-
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        //handle api routing via client side code
-        historyApiFallback: true
-    }
+    };
 };
 
 //loader, running files through babel, converting jsk to js, es5 to es6/7
